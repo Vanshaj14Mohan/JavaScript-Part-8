7d6972153e6bdd453c0ecdb90b5dfbd7f36c8a34
Vanshaj P Mohan <VanshajKumar145@gmail.com>
2025-02-12 23:44:08 +0530

    Mini Activity

--- Introduction/app.js
+++ Introduction/app.js
@@ -1,8 +1,10 @@
-let oldImg = document.getElementsByClassName("oldImg")
+// let oldImg = document.getElementsByClassName("oldImg")
 
-for(let i =0; i<oldImg.length; i++){
-    oldImg[i].src = "assets/spiderman_img.png"
-    // console.dir(oldImg[i]);
-    // console.dir(oldImg[i].src);
-    console.dir(`Value of image number ${i} is changed`);
-}
+// for(let i =0; i<oldImg.length; i++){
+//     oldImg[i].src = "assets/spiderman_img.png"
+//     // console.dir(oldImg[i]);
+//     // console.dir(oldImg[i].src);
+//     console.dir(`Value of image number ${i} is changed`);
+// }
+
+//eg selecting all paragraph tag 
