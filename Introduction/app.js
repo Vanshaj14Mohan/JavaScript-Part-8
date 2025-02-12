b1822d29447b7999553dc8ef89addb9a8df7318c
Vanshaj P Mohan <VanshajKumar145@gmail.com>
2025-02-12 23:58:48 +0530

    files

--- Introduction/app.js
+++ Introduction/app.js
@@ -1,10 +1,12 @@
 // let oldImg = document.getElementsByClassName("oldImg")
-
 // for(let i =0; i<oldImg.length; i++){
 //     oldImg[i].src = "assets/spiderman_img.png"
 //     // console.dir(oldImg[i]);
 //     // console.dir(oldImg[i].src);
 //     console.dir(`Value of image number ${i} is changed`);
 // }
-
 //eg selecting all paragraph tag 
+console.dir(document.querySelector("p"));
+console.dir(document.querySelector("#description"));
+console.dir(document.querySelector(".oldImg"));
+
